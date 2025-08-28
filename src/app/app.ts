72ba06f87f30281e118131516670858d6764e8f0
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 15:04:03 -0300

    add control open and close cart

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { ShoppingCart } from './components/shopping-cart/shopping-cart';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, ShoppingCart],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('projeto-final-angular-I-turma-1455');
+  protected readonly title = signal('projeto final angular I turma 1455');
+
+  isCartOpened = false;
+  onCartOpened(isOpened: boolean) {
+    this.isCartOpened = isOpened;
+  }
+
+  onCartClosed(closed: boolean) {
+    this.isCartOpened = closed;
+  }
 }
