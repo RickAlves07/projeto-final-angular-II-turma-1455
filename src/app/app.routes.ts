940a9fb17a9438a0227c5523bb0258ab7569aec9
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 00:26:26 -0300

    init setup and add components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { ProductDetails } from './pages/product-details/product-details';
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Checkout } from './pages/checkout/checkout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', redirectTo: '' },
+  { path: 'product-detail:id', component: ProductDetails },
+  { path: 'checkout', component: Checkout },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
