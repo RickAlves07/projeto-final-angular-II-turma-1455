a7b7be479dc864411730b2bbad5051442e97a0c1
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 18:57:28 -0300

    ajusts product-card

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,15 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { IProduct } from '../../models/interfaces/iproduct';
 import { RouterModule } from '@angular/router';
-import { CurrencyBRLPipe } from "../../pipes/currency-brl.pipe";
+import { CurrencyBRLPipe } from '../../pipes/currency-brl.pipe';
 import { CommonModule } from '@angular/common';
+import * as CartActions from '../../store/cart.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-product-card',
   imports: [RouterModule, CurrencyBRLPipe, CommonModule],
   templateUrl: './product-card.html',
-  styleUrl: './product-card.scss'
+  styleUrl: './product-card.scss',
 })
 export class ProductCard {
- @Input() product! : IProduct;
+  @Input() product!: IProduct;
+  private store = inject(Store);
+
+  addToCart() {
+    this.store.dispatch(CartActions.addProductToCart({ product: this.product }));
+    console.log('Product added to cart:', 'productId:', this.product?.id);
+  }
 }
