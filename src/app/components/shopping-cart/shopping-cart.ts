cf90c06637bd61329eec6425687b3ee99528b243
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 18:57:49 -0300

    ajusts shopping-cart

--- src/app/components/shopping-cart/shopping-cart.ts
+++ src/app/components/shopping-cart/shopping-cart.ts
@@ -3,8 +3,9 @@ import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { removeAllQuantityProductFromCart, removeProductFromCart } from '../../store/cart.actions';
+import * as CartActions from '../../store/cart.actions';
 import { CurrencyBRLPipe } from "../../pipes/currency-brl.pipe";
+import { IProduct } from '../../models/interfaces/iproduct';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -15,9 +16,9 @@ import { CurrencyBRLPipe } from "../../pipes/currency-brl.pipe";
 export class ShoppingCart {
   @Input() isCheckout: boolean = false;
   @Output() cartClosed = new EventEmitter<boolean>();
-  isCartOpened = true;
   private store = inject(Store);
 
+  isCartOpened = true;
   cartItems$: Observable<any[]>;
   totalItems$: Observable<number>;
   total$: Observable<number>;
@@ -28,13 +29,19 @@ export class ShoppingCart {
     this.total$ = this.store.select((state) => state.cart.total);
   }
 
+  addToCart(product: IProduct){
+    // product.quantity = 1;
+    this.store.dispatch(CartActions.addProductToCart({ product: product }));
+    console.log('Product added to cart:', 'productId:', product?.id);
+  }
+
   subtractItem(productId: number) {
-    this.store.dispatch(removeProductFromCart({ productId }));
+    this.store.dispatch(CartActions.removeProductFromCart({ productId }));
     console.log('Product subtracted from cart:', 'productId:', productId);
   }
 
   removeItem(productId: number) {
-    this.store.dispatch(removeAllQuantityProductFromCart({ productId }));
+    this.store.dispatch(CartActions.removeAllQuantityProductFromCart({ productId }));
     console.log('Product removed from cart:', 'productId:', productId);
   }
 
