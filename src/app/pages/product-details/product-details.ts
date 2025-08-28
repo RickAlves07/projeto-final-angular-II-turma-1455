55880668848ece19d11ca1252ffc12731fc021c1
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 15:07:16 -0300

    ajusts product detail

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products-service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, switchMap } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as CartActions from '../../store/cart.actions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-details.html',
-  styleUrl: './product-details.scss'
+  styleUrl: './product-details.scss',
 })
 export class ProductDetails {
+  private route = inject(ActivatedRoute);
+  private productService = inject(ProductsService);
+  private store = inject(Store);
 
+  product = toSignal(
+    this.route.params.pipe(
+      map((params) => params['id']),
+      switchMap((id) => this.productService.getProductById(id))
+    )
+  );
+
+  addToCart() {
+    this.store.dispatch(
+      CartActions.addProductToCart({ product: this.product() })
+    );
+    console.log('Product added to cart:', 'productId:', this.product()?.id);
+  }
 }
