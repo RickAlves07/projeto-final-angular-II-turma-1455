ead2d41fe32ab971d9655423a1889c175c48cdf5
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 18:58:08 -0300

    ajusts cart.reducer

--- src/app/store/cart.reducer.ts
+++ src/app/store/cart.reducer.ts
@@ -15,7 +15,7 @@ export const cartReducer = createReducer(
     let newItems;
     if (existingItem) {
       newItems = state.items.map((item) =>
-        item.id === product.id ? { ...item, quantity: (item.quantity || 1) + (product.quantity || 1) } : item
+        item.id === product.id ? { ...item, quantity: (item.quantity || 1) + (1) } : item
       );
     } else {
       newItems = [
