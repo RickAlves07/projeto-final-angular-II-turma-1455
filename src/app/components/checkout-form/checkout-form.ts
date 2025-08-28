e0969759b1bb80148c08e7ddfda7f6bd29189481
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 18:57:17 -0300

    add checkout-form

---
+++ src/app/components/checkout-form/checkout-form.ts
@@ -0,0 +1,58 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { digitsOnlyValidator } from '../../shared/validators/digits-only.validator';
+
+@Component({
+  selector: 'app-checkout-form',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './checkout-form.html',
+  styleUrl: './checkout-form.scss'
+})
+export class CheckoutForm {
+  private fb = new FormBuilder();
+
+  regexMes = /^(0?[1-9]|1[0-2])$/;
+  regexAno = /^(\d{4})$/;
+
+  checkoutForm: FormGroup = this.fb.group({
+    nomeCompleto: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    endereco: ['', [Validators.required, Validators.minLength(5)]],
+
+    nomeNoCartao: ['', [Validators.required, Validators.minLength(3)]],
+    numeroCartao: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(16),
+        Validators.maxLength(16),
+        digitsOnlyValidator(),
+      ],
+    ],
+    codigoSeguranca: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(3),
+        digitsOnlyValidator(),
+      ],
+    ],
+    validadeMes: ['', [Validators.required, Validators.pattern(this.regexMes)]],
+    validadeAno: ['', [Validators.required, Validators.pattern(this.regexAno)]],
+  });
+
+  submittedData = signal<any | null>(null);
+
+  onSubmit() {
+    this.checkoutForm.markAllAsTouched();
+    if (this.checkoutForm.valid) {
+      this.submittedData.set(this.checkoutForm.value);
+    }
+  }
+
+  control(name: string): AbstractControl {
+    return this.checkoutForm.get(name)!;
+  }
+}
