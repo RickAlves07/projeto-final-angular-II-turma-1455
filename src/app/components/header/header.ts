b7b4f6f3c515e4cc2d1826900fe37b117269b2b9
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 15:04:26 -0300

    ajusts header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartStatus } from '../cart-status/cart-status';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CartStatus, RouterModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
-
+  @Input() isCartOpened: boolean = false;
+  @Output() cartOpened = new EventEmitter<boolean>();
+  openCart() {
+    this.isCartOpened = !this.isCartOpened;
+    this.cartOpened.emit(this.isCartOpened);
+  }
+  onCartOpened(isOpened: boolean) {
+    this.isCartOpened = isOpened;
+    this.cartOpened.emit(this.isCartOpened);
+  }
 }
