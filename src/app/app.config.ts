1ed04974579c8f2f9f8c68780cf1e58255fb707f
Rick Alves <rick_07@hotmail.com.br>
2025-08-28 15:03:05 -0300

    add store setting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { cartReducer } from './store/cart.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({ cart: cartReducer })
   ]
 };
