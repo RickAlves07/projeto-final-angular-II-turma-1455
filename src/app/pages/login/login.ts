5f7e99463fc4ef5477b603a355cb023573beb083
Rick Alves <rick_07@hotmail.com.br>
2025-09-28 00:11:56 -0300

    ajusts login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import { Router, RouterModule } from '@angular/router';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  loginForm: FormGroup = new FormGroup({});
+  submittedData = signal<any | null>(null);
 
+  constructor(authService: AuthService, router: Router) {
+    this.initLoginForm();
+  }
+
+  initLoginForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.valid) {
+      this.submittedData.set(this.loginForm.value);
+    }
+  }
+
+  control(name: string): AbstractControl {
+    return this.loginForm.get(name)!;
+  }
 }
