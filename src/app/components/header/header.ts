697b173a5779c2539a8cc3476fab0d1d964f5947
Rick Alves <rick_07@hotmail.com.br>
2025-09-29 15:49:31 -0300

    ajusts header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartStatus } from '../cart-status/cart-status';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-header',
@@ -11,18 +12,37 @@ import { RouterModule } from '@angular/router';
 export class Header {
   @Input() isCartOpened: boolean = false;
   @Output() cartOpened = new EventEmitter<boolean>();
-  openCart() {
-    this.isCartOpened = !this.isCartOpened;
-    this.cartOpened.emit(this.isCartOpened);
-  }
 
   logedTexts = {
     text: 'Login ',
     icon: 'bi-box-arrow-in-right',
   }
 
+  constructor(private authService: AuthService) {}
+
+  get isLoggedIn(): boolean {
+    const isLoggedIn = this.authService.isAuthenticated();
+    if (isLoggedIn) {
+      this.logedTexts.text = 'Logout ';
+      this.logedTexts.icon = 'bi-box-arrow-in-left';
+    } else {
+      this.logedTexts.text = 'Login ';
+      this.logedTexts.icon = 'bi-box-arrow-in-right';
+    }
+
+    return isLoggedIn;
+  }
+  openCart() {
+    this.isCartOpened = !this.isCartOpened;
+    this.cartOpened.emit(this.isCartOpened);
+  }
+
   onCartOpened(isOpened: boolean) {
     this.isCartOpened = isOpened;
     this.cartOpened.emit(this.isCartOpened);
+  }
+
+  logoutCurrentUser() {
+    this.authService.logout();
   }
 }
