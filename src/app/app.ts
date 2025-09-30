42da994f7cfc2928874719e697db7970d27df005
Rick Alves <rick_07@hotmail.com.br>
2025-09-30 00:40:49 -0300

    ajusts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { ShoppingCart } from './components/shopping-cart/shopping-cart';
+import { AuthService } from './services/auth-service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,20 @@ import { ShoppingCart } from './components/shopping-cart/shopping-cart';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('projeto final angular I turma 1455');
+export class App  implements OnInit{
+  protected readonly title = signal('projeto final angular II turma 1455');
 
   isCartOpened = false;
+
+  constructor(private authService: AuthService, private translateService: TranslateService) {
+    const savedLang = localStorage.getItem('selectedLanguage')?? 'pt-br';
+    this.translateService.use(savedLang);
+  }
+
+  ngOnInit(): void {
+    this.authService.checkLoginExpiration();
+  }
+
   onCartOpened(isOpened: boolean) {
     this.isCartOpened = isOpened;
   }
